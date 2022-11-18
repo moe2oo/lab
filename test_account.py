e86acea75fa61de241bad789ddea5d2965fbbeda
moe_oo <moo@unomaha.edu>
2022-11-18 15:44:02 -0600

    Added sixth file

--- test_account.py
+++ test_account.py
@@ -13,32 +13,29 @@ class Test:
         assert self.p1.get_balance() == 0
 
     def test_deposit(self):
-        assert self.p1.deposit(500) == True
+        assert self.p1.deposit(500) is True
         assert self.p1.get_balance() == 500
-        assert self.p1.deposit(-30) == False
+        assert self.p1.deposit(-30) is False
         assert self.p1.get_balance() == 500
-        assert self.p1.deposit(0) == False
+        assert self.p1.deposit(0) is False
         assert self.p1.get_balance() == 500
 
-        assert self.p1.deposit(3.0) == True
-        assert self.p1.get_balance() == 503.0
-        assert self.p1.deposit(-30) == False
-        assert self.p1.get_balance() == 503.0
-        assert self.p1.deposit(0) == False
-        assert self.p1.get_balance() == 503.0
+        assert self.p1.deposit(3.5) is True
+        assert self.p1.get_balance() == pytest.approx(503.5, abs = 0.001)
+
 
     def test_withdraw(self):
 
-        assert self.p1.withdraw(600) == False
+        assert self.p1.withdraw(600) is False
         assert self.p1.get_balance() == 0
-        assert self.p1.withdraw(0) == False
+        assert self.p1.withdraw(0) is False
         assert self.p1.get_balance() == 0
-        assert self.p1.withdraw(2) == False
-        assert self.p1.get_balance() ==0
-        assert self.p1.withdraw(-2) == False
+        assert self.p1.withdraw(-2) is False
         assert self.p1.get_balance() == 0
 
         self.p1.deposit(30)
-        assert self.p1.withdraw(2.5) == True
+        assert self.p1.withdraw(2.5) is True
         assert self.p1.get_balance() == pytest.approx(27.5, abs = 0.001)
+        assert self.p1.withdraw(1) is True
+        assert self.p1.get_balance() == pytest.approx(26.5, abs = 0.001)
 
