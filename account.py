c0b50ba9348eebcfdffb63fb98b9e4efb103644a
moe_oo <moo@unomaha.edu>
2022-11-18 14:14:48 -0600

    Added second file

--- account.py
+++ account.py
@@ -1,10 +1,20 @@
 class Account:
-    def __init__(self, name, balance):
+    def __init__(self, name: str, balance: 0.0) -> None:
+        """
+        Constructor to create initial stae of a person object
+        :param name: person's name
+        :param balance: person's balance
+        """
         self.__account_name = name
         self.__account_balance = balance
 
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        """
+        Compute balance after deposit
+        :param amount: deposit amount
+        :return: result on whether transaction is successful or unsuccessful
+        """
         if amount <= 0:
             return False
         else:
@@ -12,7 +22,12 @@ class Account:
             return True
 
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        """
+        Compute balance after withdraw
+        :param amount: withdraw amount
+        :return: result on whether transaction is successful or unsuccessful
+        """
         if amount > self.__account_balance or amount <= 0:
             return False
         else:
@@ -20,9 +35,17 @@ class Account:
             return True
 
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Method to access a person's balance
+        :return: person's balance
+        """
         return self.__account_balance
 
 
-    def get_name(self):
+    def get_name(self) -> str:
+        """
+        Method to access a person's name
+        :return: person's name
+        """
         return self.__account_name
